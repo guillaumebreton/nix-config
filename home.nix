08bcc7626b7cae8e5b16d6b11cce28256b4e6576
Guillaume Breton <breton.gy@gmail.com>
2021-07-07 16:30:32 +0200

    Install qmk and python

--- home.nix
+++ home.nix
@@ -61,8 +61,14 @@ in {
 		ripgrep # Replacement for grep
 		starship # Fancy shell that works with zsh
 		trash-cli # a replacement for rm
-		qmk # firware setup
-		python39 # good old snake
+		python39
+		python39Packages.pip
+		qmk
+		dfu-util
+		dfu-programmer
+		avrdude
+		arm-none-eabi-gcc
+				avr-gcc
 
 	] ++ gitTools ;
 
