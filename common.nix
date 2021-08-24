3fb0bc94c41048fd2053dcdf94ec91bc019bb7b3
Guillaume Breton <breton.gy@gmail.com>
2021-08-24 15:59:22 +0200

    Add profiles

--- common.nix
+++ common.nix
@@ -25,10 +25,6 @@ in {
 	# Let Home Manager install and manage itself.
 	programs.home-manager.enable = true;
 
-	# Home Manager needs a bit of information about you and the
-	# paths it should manage.
-	home.username = "guillaume";
-	home.homeDirectory = "/Users/guillaume";
 
 	home.stateVersion = "21.11";
 
---
+++ profiles/kitsune.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+let
+	nigpkgsRev = "nixpkgs-unstable";
+	pkgs = import (fetchTarball "https://github.com/nixos/nixpkgs/archive/${nigpkgsRev}.tar.gz") {};
+
+	# Import other Nix files
+	imports = [
+		../common.nix
+	];
+
+in {
+
+	inherit imports;
+
+	# Home Manager needs a bit of information about you and the
+	# paths it should manage.
+	home.username = "guillaumebreton";
+	home.homeDirectory = "/Users/guillaumebreton";
+
+}
