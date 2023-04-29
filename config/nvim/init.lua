db7bf706c9aa7f4b5574e6a1085a417a99ca5ff5
Guillaume Breton <breton.gy@gmail.com>
2023-04-29 19:36:37 +0200

    Add some settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,38 @@
 require("yak");
 vim.cmd [[colorscheme nightfox]]
+
+----------------
+--- SETTINGS ---
+----------------
+
+-- disable netrw at the very start of our init.lua, because we use nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true                      -- Enable 24-bit RGB colors
+
+vim.opt.number = true                             -- Show line numbers
+vim.opt.showmatch = true                          -- Highlight matching parenthesis
+vim.opt.splitright = true                         -- Split windows right to the current windows
+vim.opt.splitbelow = true                         -- Split windows below to the current windows
+vim.opt.autowrite = true                          -- Automatically save before :next, :make etc.
+vim.opt.autochdir = true                          -- Change CWD when I open a file
+
+vim.opt.mouse = 'a'                               -- Enable mouse support
+vim.opt.clipboard = 'unnamedplus'                 -- Copy/paste to system clipboard
+vim.opt.swapfile = false                          -- Don't use swapfile
+vim.opt.ignorecase = true                         -- Search case insensitive...
+vim.opt.smartcase = true                          -- ... but not it begins with upper case
+vim.opt.completeopt = 'menuone,noinsert,noselect' -- Autocomplete options
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "undo"
+
+-- Indent Settings
+-- I'm in the Spaces camp (sorry Tabs folks), so I'm using a combination of
+-- settings to insert spaces all the time.
+vim.opt.expandtab = true  -- expand tabs into spaces
+vim.opt.shiftwidth = 2    -- number of spaces to use for each step of indent.
+vim.opt.tabstop = 2       -- number of spaces a TAB counts for
+vim.opt.autoindent = true -- copy indent from current line when starting a new line
+vim.opt.wrap = true
