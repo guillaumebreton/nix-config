aebdab151a930691287f2364ef0078b174ee4875
Guillaume Breton <breton.gy@gmail.com>
2023-04-29 21:36:47 +0200

    Try to use unstable for home manager

--- flake.nix
+++ flake.nix
@@ -4,20 +4,39 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Specify the channel to use for Home Manager and Nixpkgs.
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
+
+
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    {
+  outputs = { nixpkgs, home-manager, nixpkgs-unstable, ... }:
+    let
+      system = "x86_64-darwin";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+
+      lib = nixpkgs.lib;
+    in {
+      # Use the provided nixpkgs and home-manager.
       homeConfigurations.bunraku = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-darwin";
+
+        inherit pkgs;
         modules = [
           ./profiles/bunraku.nix
         ];
+        # use nix unstable channel in home-manager.
+        extraSpecialArgs = {
+            inherit nixpkgs-unstable;
+        };
       };
     };
 }
-
--- modules/nvim.nix
+++ modules/nvim.nix
@@ -15,6 +15,6 @@
         pkgs.vimPlugins.telescope-nvim
         (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [ p.c p.rust p.go p.javascript p.java ]))
         pkgs.vimPlugins.nightfox-nvim
-        pkgs.vimplugin-lsp-zero.nvim
+        # pkgs.vimPlugins.lsp-zero-nvim
     ];
 }
