82e6717e4ec5bd284ae32488d5bf36481b4e3dd4
Guillaume Breton <breton.gy@gmail.com>
2023-04-30 21:26:04 +0200

    Add tree and nix

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,24 +1,35 @@
 # nix module to install neovim
 { config, pkgs, ... }:
 {
+    # add config
     xdg.configFile."nvim" = {
         source = ../config/nvim;
         target = "nvim";
     };
 
+    # enable neovim
     programs.neovim = {
         enable = true;
         viAlias = true;
         vimAlias = true;
     };
+
+    # install plugin
    	programs.neovim.plugins = with pkgs.vimPlugins; [
 
         # fuzzy finding
         telescope-nvim
 
         # treesitter
-        (nvim-treesitter.withPlugins (p: [ p.c p.rust p.go p.javascript p.java ]))
+        (nvim-treesitter.withPlugins (p: [ p.c p.rust p.go p.javascript p.java, p.nix ]))
+        # Syntax aware text-objects, select, move, swap, and peek support
         nvim-treesitter-textobjects
+
+        # Github copilot
+        copilot-vim
+
+        # vim tree lua
+        nvim-tree-lua
 
         # lsp
         nvim-lspconfig
