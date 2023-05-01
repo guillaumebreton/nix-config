4d0a5ab5ce44ea3786f32f75f2884e854b153c0c
Guillaume Breton <breton.gy@gmail.com>
2023-05-01 14:43:22 +0200

    Fix dev function

--- modules/shell.nix
+++ modules/shell.nix
@@ -33,7 +33,7 @@ let
 		grc="git rebase --continue";
 		gpf="git push --force-with-lease";
 
-    # github pr 
+    # github pr
     gpr="gh pr view --web || gh pr create --fill --web";
 
 
@@ -122,34 +122,34 @@ in {
 			eval "$(direnv hook zsh)"
 
 
-      # start devlopement enviroment
-      dev() {
-        # If no argument is given, list all the workspaces
-        selected=$1
-        if [ -z $1 ]; then
-          selected=`ls ~/Workspaces/ | fzf` 
-        fi
+            # start devlopement enviroment
+            dev(){
+                # If no argument is given, list all the workspaces
+                selected=$1
+                if [ -z $1 ]; then
+                selected=`ls ~/Workspaces/ | fzf`
+                fi
 
-        # ctrl-c pressed
-        if [ -z $selected ]; then
-          exit 0
-        fi
-      
-        WORKING_DIRECTORY=$(cdpath=(. ~/Workspaces) cd $selected > /dev/null 2>&1 && pwd)
-        echo "working directory: $WORKING_DIRECTORY"
+                # ctrl-c pressed
+                if [ -z $selected ]; then
+                exit 0
+                fi
 
-        # Switch session
-        session_name=`echo $selected | sed 's/\./_/g'`
-        if [ -z "$TMUX" ]; then
-          tmux new-session -As $session_name -c $WORKING_DIRECTORY
-        else
-          if ! tmux has-session -t $session_name 2>/dev/null; then
-            TMUX= tmux new-session -ds $session_name -c $WORKING_DIRECTORY
-          fi
-          echo "switching to $session_name session"
-          tmux switch-client -t $session_name
-        fi
-      }
+                WORKING_DIRECTORY=$(cdpath=(. ~/Workspaces) cd $selected > /dev/null 2>&1 && pwd)
+                echo "working directory: $WORKING_DIRECTORY"
+
+                # Switch session
+                session_name=`echo $selected | sed 's/\./_/g'`
+                if [ -z "$TMUX" ]; then
+                tmux new-session -As $session_name -c $WORKING_DIRECTORY
+                else
+                if ! tmux has-session -t $session_name 2>/dev/null; then
+                    TMUX= tmux new-session -ds $session_name -c $WORKING_DIRECTORY
+                fi
+                echo "switching to $session_name session"
+                tmux switch-client -t $session_name
+                fi
+            }
 
 
 			tad(){
