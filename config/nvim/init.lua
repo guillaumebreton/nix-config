00f2546800551baba4795ed311139e5fabca267d
Guillaume Breton <breton.gy@gmail.com>
2023-05-01 12:39:36 +0200

    Move mappings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -36,3 +36,48 @@ vim.opt.shiftwidth = 2    -- number of spaces to use for each step of indent.
 vim.opt.tabstop = 2       -- number of spaces a TAB counts for
 vim.opt.autoindent = true -- copy indent from current line when starting a new line
 vim.opt.wrap = true
+
+
+----------------------------
+-- MAPPINGS
+----------------------------
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- move the screen one page downward and center the cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- move the screen one page backward and center the cursor
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- unknow
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- file tree mapping
+vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>f', ':NvimTreeFindFileToggle<CR>', { noremap = true })
+
+-- Better split switching
+vim.keymap.set('', '<C-j>', '<C-W>j')
+vim.keymap.set('', '<C-k>', '<C-W>k')
+vim.keymap.set('', '<C-h>', '<C-W>h')
+vim.keymap.set('', '<C-l>', '<C-W>l')
+
+-- Fast saving
+vim.keymap.set('n', '<Leader>w', ':write!<CR>')
+vim.keymap.set('n', '<Leader>q', ':q!<CR>', { silent = true })
+vim.opt.number = true        -- Show line numbers
+vim.opt.showmatch = true     -- Highlight matching parenthesis
+vim.opt.splitright = true    -- Split windows right to the current windows
+vim.opt.splitbelow = true    -- Split windows below to the current windows
+vim.opt.autowrite = true     -- Automatically save before :next, :make etc.
+vim.opt.autochdir = true     -- Change CWD when I open a file
+
+-- diagnostics
+vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>ds', vim.diagnostic.setqflist)
