b44ca11746f2cfc3487e63ccbe9fcdc69fb34893
Guillaume Breton <breton.gy@gmail.com>
2023-05-01 14:37:14 +0200

    Add alias

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/zsh
 # This script is used to start the development environment
 
 
@@ -8,16 +8,25 @@ tm() {
   if [ -z $1 ]; then
     selected=`ls ~/Workspaces/ | fzf` 
   fi
-  selected=`echo $selected | sed 's/\./_/g'`
-  echo $selected
+
+  # ctrl-c pressed
+  if [ -z $selected ]; then
+    exit 0
+  fi
+  
+  WORKING_DIRECTORY=$(cdpath=(. ~/Workspaces) cd $selected > /dev/null 2>&1 && pwd)
+  echo "working directory: $WORKING_DIRECTORY"
+
+  # Switch session
+  session_name=`echo $selected | sed 's/\./_/g'`
   if [ -z "$TMUX" ]; then
-    tmux new-session -As "$selected"
+    tmux new-session -As $session_name -c $WORKING_DIRECTORY
   else
-    if ! tmux has-session -t $selected 2>/dev/null; then
-      TMUX= tmux new-session -ds "$selected"
+    if ! tmux has-session -t $session_name 2>/dev/null; then
+      TMUX= tmux new-session -ds $session_name -c $WORKING_DIRECTORY
     fi
-    echo "switching to $selected session"
-    tmux switch-client -t "$selected"
+    echo "switching to $session_name session"
+    tmux switch-client -t $session_name
   fi
 }
 
