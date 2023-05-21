0196625131339bb927dd312bc7492f4f8efa7a3c
Guillaume Breton <breton.gy@gmail.com>
2023-05-21 11:03:31 +0200

    Use find_files instead of git_file for c-p

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -238,7 +238,7 @@ require('nvim-treesitter.configs').setup {
 
 -- telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pb', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
