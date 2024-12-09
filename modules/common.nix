bf193414b938020742f5c859c12bd4263cb05c1d
Guillaume Breton <breton.gy@gmail.com>
2024-12-09 12:51:56 +0100

    Add back alacritty

--- modules/common.nix
+++ modules/common.nix
@@ -5,6 +5,7 @@ let
 
 	# Import other Nix files
 	imports = [
+	   ./alacritty.nix
 		./git.nix
 		./shell.nix
 		./starship.nix
@@ -36,26 +37,7 @@ in {
 		# TERMINAL = "alacritty";
 	};
 
-	home.activation =  {
-      copyApplications = let
-        apps = pkgs.buildEnv {
-          name = "home-manager-applications";
-          paths = config.home.packages;
-          pathsToLink = "/Applications";
-        };
-      in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        baseDir="$HOME/Applications/Home Manager Apps"
-        if [ -d "$baseDir" ]; then
-          rm -rf "$baseDir"
-        fi
-        mkdir -p "$baseDir"
-        for appFile in ${apps}/Applications/*; do
-          target="$baseDir/$(basename "$appFile")"
-          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-        done
-      '';
-    };
+
 
 
 	home.packages = with pkgs; [
