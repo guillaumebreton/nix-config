f61596fa6de79b688e12935e42d7cf7a2255cb8a
Guillaume Breton <breton.gy@gmail.com>
2024-12-09 13:33:09 +0100

    Use catppuccin in alacritty

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -1,31 +1,26 @@
 { config, lib, pkgs, ... }:
 
 {
-	programs.alacritty = {
-	enable = true;
-	settings = {
-		env = {
-			"TERM" = "xterm-256color";
-		};
-		window = {
-			padding.x = 5;
-			padding.y = 5;
-			title = "bunraku";
-		};
-		font = {
-			size = 13.0;
-            offset = {
-                y = 3;
-            };
-			# use_thin_strokes = true;
-			normal.family = "Berkeley Mono";
-			bold.family = "Berkeley Mono";
-		};
-		cursor.style = "Beam";
-		scrolling = {
-			history = 10000;
-		};
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      env = { "TERM" = "xterm-256color"; };
+      window = {
+        padding.x = 5;
+        padding.y = 5;
+        title = "bunraku";
+      };
+      font = {
+        size = 13.0;
+        offset = { y = 3; };
+        # use_thin_strokes = true;
+        normal.family = "Berkeley Mono";
+        bold.family = "Berkeley Mono";
+      };
+      cursor.style = "Beam";
+      scrolling = { history = 10000; };
+      import = [ "~/.config/alacritty/catppuccin-mocha.toml" ];
 
-	};
+    };
   };
 }
