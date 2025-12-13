4e0cab1eb84ff06bf280b931ad5a7f51f1918a21
Guillaume Breton <breton.gy@gmail.com>
2025-12-13 17:34:09 +0100

    remove git and tools

--- modules/git.nix
+++ modules/git.nix
@@ -1,54 +1,49 @@
 # Git settings
 
-{  pkgs, ... }:
+{
+  programs.git = {
+    enable = true;
+    userName = "Guillaume Breton";
+    userEmail = "breton.gy@gmail.com";
 
-let
-	vscode = pkgs.vscode;
-in {
-	programs.git = {
-		package = pkgs.gitAndTools.gitFull;
-		enable = true;
-		userName = "Guillaume Breton";
-		userEmail = "breton.gy@gmail.com";
+    # Replaces ~/.gitignore
+    ignores = [
+      ".cache/"
+      ".DS_Store"
+      ".idea/"
+      "*.swp"
+      "built-in-stubs.jar"
+      "dumb.rdb"
+      ".elixir_ls/"
+      ".vscode/"
+      "npm-debug.log"
+    ];
 
-		# Replaces ~/.gitignore
-		ignores = [
-			".cache/"
-			".DS_Store"
-			".idea/"
-			"*.swp"
-			"built-in-stubs.jar"
-			"dumb.rdb"
-			".elixir_ls/"
-			".vscode/"
-			"npm-debug.log"
-		];
+    # Replaces aliases in ~/.gitconfig
+    aliases = {
+      lg = "log --graph -20 --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(bold blue)%an %Creset: %s %Cgreen(%cr)' --abbrev-commit --date=relative";
+      l = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(bold blue)%an %Creset: %s %Cgreen(%cr)'";
+      ll = "log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --numstat";
+      st = "status --short";
+    };
 
-		# Replaces aliases in ~/.gitconfig
-		aliases = {
-			lg = "log --graph -20 --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(bold blue)%an %Creset: %s %Cgreen(%cr)' --abbrev-commit --date=relative";
-			l = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(bold blue)%an %Creset: %s %Cgreen(%cr)'";
-			ll = "log --pretty=format:'%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]' --decorate --numstat";
-			st = "status --short";
-		};
-
-		# Global Git config
-		extraConfig = {
-			core = {
-			editor = "vi";
-			pager = "delta --dark";
-			whitespace = "trailing-space,space-before-tab";
-			};
-			push = {
-				default = "current";
-				followTags = "true";
-			};
-			branch = {
-				autosetuprebase = "always";
-			};
-			rebase = {
-				autosquash = "true";
-			};
-		};
-	};
+    # Global Git config
+    extraConfig = {
+      core = {
+        editor = "vi";
+        pager = "delta --dark";
+        whitespace = "trailing-space,space-before-tab";
+      };
+      push = {
+        default = "current";
+        followTags = "true";
+      };
+      branch = {
+        autosetuprebase = "always";
+      };
+      rebase = {
+        autosquash = "true";
+      };
+    };
+  };
 }
